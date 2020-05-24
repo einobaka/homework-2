910fb965171efabcb1f659b250eccd52b33de640
einobaka <61556425+einobaka@users.noreply.github.com>
2020-05-23 20:54:00 -0700

    added JS for later development

--- script/script.js
+++ script/script.js
@@ -1,10 +1,20 @@
 // About page script
 
-$("#content").text("is this even working")
+// $("#about").on("click", function () {
 
+//     const divRow = $("<div>");
+//     const divCol1 = $("<div>");
+//     const divCol2 = $("<div>");
+
+//     $("#content").append(divRow);
+//     $(divRow).attr("class", "row").append(divCol1).append(divCol2);
+
+//     $(divCol1).attr("class", "col");
+//     $(divCol2).attr("class", "col");
+
+//     $(divCol1).text("test")
+// });
 
 // Contact page script
-
-
 
 // Portfolio page script
